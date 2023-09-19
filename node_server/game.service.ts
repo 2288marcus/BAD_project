9391bfe7a1f2431d4bdc61b6e9430f60e73dd281
TS23hk <137588045+TS23hk@users.noreply.github.com>
2023-09-19 23:27:25 +0800

    remove unnessary try catch in service

--- node_server/game.service.ts
+++ node_server/game.service.ts
@@ -1,32 +1,28 @@
 import { Knex } from "knex";
 
 export class GameService {
-  constructor(private knex: Knex) { }
+  constructor(private knex: Knex) {}
   async gameRank() {
-    try {
-      let result = await this.knex.raw(
-        ` select * from game_record join game on game.id=game_record.game_id where mode='5Cup' order by user_score desc limit 10 ;`
-      );
-      let fiveCupdata = result.rows;
+    let result = await this.knex.raw(
+      ` select * from game_record join game on game.id=game_record.game_id where mode='5Cup' order by user_score desc limit 10 ;`
+    );
+    let fiveCupdata = result.rows;
 
-      let result2 = await this.knex.raw(
-        ` select * from game_record join game on game.id=game_record.game_id where mode='1Minute' order by user_score desc limit 10 ;`
-      );
-      let oneMinuteData = result2.rows;
+    let result2 = await this.knex.raw(
+      ` select * from game_record join game on game.id=game_record.game_id where mode='1Minute' order by user_score desc limit 10 ;`
+    );
+    let oneMinuteData = result2.rows;
 
-      return { fiveCupdata, oneMinuteData };
-    } catch (error) {
-      console.log(error);
-    }
+    return { fiveCupdata, oneMinuteData };
   }
 
   async gameResult(username: string, score: number) {
-    try {
-      let result = await this.knex("game_record").insert({ username, user_score: score, game_id: 1 })
+    let result = await this.knex("game_record").insert({
+      username,
+      user_score: score,
+      game_id: 1,
+    });
 
-      return { result };
-    } catch (error) {
-      console.log(error);
-    }
+    return { result };
   }
 }
